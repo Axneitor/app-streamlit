cb34fe2df936178680b5af63fa47b245bd841896
Axneitor <alex.mora.axotmail.com>
2025-05-22 20:57:03 -0500

    Cambios

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 
-URL = "https://model-flask-docker-r9zo.onrender.com/predict/"
+#URL = "https://model-flask-docker-r9zo.onrender.com/predict/"
+URL = "https://reroes-ml01-latest-9hr1.onrender.com/predict/"
